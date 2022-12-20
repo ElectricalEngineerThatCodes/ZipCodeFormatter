b9694b494921c2d84265a2b0ae69939f83cdb85d
ElectricalEngineerThatCodes <atown622@gmail.com>
2022-12-19 21:53:06 -0500

    Initial Commit of creating downloading excel

---
+++ app.js
@@ -0,0 +1,35 @@
+const { getWorkbook } = require('./ZipFormatter.js');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const arr = [1, 2, 3, 4, 5];
+
+app.get('/donors', async (req, res) => {
+  const fileName = 'FormatForMailing2.xlsx';
+
+  res.setHeader(
+    'Content-Type',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+  );
+
+  res.setHeader('Content-Disposition', 'attachment; filename=' + fileName);
+
+  const workbook = await getWorkbook();
+
+  await workbook.xlsx.write(res);
+  res.status(200).end();
+  //   res.end();
+});
+
+app.get('/users/atown', (req, res) => {
+  res.send('Andrew Townley');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
